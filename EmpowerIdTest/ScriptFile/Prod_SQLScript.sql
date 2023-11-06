0bb94edd847d675a3f21427eea2636beabf53a2c
Shweta <stdev.net@gmail.com>
2023-11-06 17:14:34 +0530

    Added Script file

---
+++ EmpowerIdTest/ScriptFile/Prod_SQLScript.sql
@@ -0,0 +1,76 @@
+﻿/****** Object:  Table [dbo].[Categories]    Script Date: 11/6/2023 5:12:16 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Categories](
+	[Category_Id] [uniqueidentifier] NOT NULL,
+	[Category_Name] [nvarchar](255) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Category_Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Order_Products]    Script Date: 11/6/2023 5:12:16 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Order_Products](
+	[Order_Id] [uniqueidentifier] NOT NULL,
+	[Product_Id] [uniqueidentifier] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Order_Id] ASC,
+	[Product_Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Orders]    Script Date: 11/6/2023 5:12:16 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Orders](
+	[Order_Id] [uniqueidentifier] NOT NULL,
+	[Order_Date] [date] NULL,
+	[Customer_Name] [nvarchar](255) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Order_Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Products]    Script Date: 11/6/2023 5:12:16 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Products](
+	[Product_Id] [uniqueidentifier] NOT NULL,
+	[Product_Name] [nvarchar](255) NOT NULL,
+	[Category_Id] [uniqueidentifier] NULL,
+	[Price] [decimal](10, 2) NULL,
+	[Description] [nvarchar](255) NULL,
+	[Image_Url] [nvarchar](255) NULL,
+	[Date_Added] [date] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Product_Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Order_Products]  WITH CHECK ADD FOREIGN KEY([Order_Id])
+REFERENCES [dbo].[Orders] ([Order_Id])
+GO
+ALTER TABLE [dbo].[Order_Products]  WITH CHECK ADD FOREIGN KEY([Product_Id])
+REFERENCES [dbo].[Products] ([Product_Id])
+GO
+ALTER TABLE [dbo].[Products]  WITH CHECK ADD FOREIGN KEY([Category_Id])
+REFERENCES [dbo].[Categories] ([Category_Id])
+GO
+USE [master]
+GO
+ALTER DATABASE [EmpowerID] SET  READ_WRITE 
+GO
